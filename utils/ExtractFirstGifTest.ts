03b92213bd3a477961de20618d7e3694bbdfb046
mmnitzsche <mmnitzsche@gmail.com>
2025-03-06 19:41:00 -0300

    updating the post carrousel

--- utils/ExtractFirstGifTest.ts
+++ utils/ExtractFirstGifTest.ts
@@ -1,6 +1,10 @@
 export const extractAllGifs = (content?: string): string[] => {
     if (!content) return [];
     const regex = /<img[^>]+src=["']([^"']+\.gif)["'][^>]*>/gi;
-    const matches = [...content.matchAll(regex)];
-    return matches.map(match => match[1]); // Retorna um array com todos os GIFs encontrados
+    const matches: string[] = [];
+    let match;
+    while ((match = regex.exec(content)) !== null) {
+        matches.push(match[1]);
+    }
+    return matches; // Retorna um array com todos os GIFs encontrados
 };
