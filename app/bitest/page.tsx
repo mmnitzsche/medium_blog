727505819d0d2fdf0ee3ca3bbc42c12caae3e879
mmnitzsche <mmnitzsche@gmail.com>
2025-04-24 13:32:42 -0300

    bi-test

---
+++ app/bitest/page.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+export default function page() {
+    return (
+        <>
+            <div>
+                <iframe title="Animation Studio" width="600" height="373.5" src="https://app.powerbi.com/view?r=eyJrIjoiNTIzNjk2OWYtYjU3Ny00M2FiLWJiM2MtYzEyYzdkODRlZmQyIiwidCI6ImQ2NGQ5ZTY3LTQxZjItNDAyOS1hY2M3LTE2MDYxNTcxNjYwYiJ9" frameborder="0" allowFullScreen="true"></iframe>
+            </div>
+        </>
+    );
+}
